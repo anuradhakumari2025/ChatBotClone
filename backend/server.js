c08d039bbfb7ecc56750f004dd81c01e9ada2374
anuradhakumari2025 <krianuradha4321@gmail.com>
2025-08-31 22:25:37 +0530

    Added dist of frontend in backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const http = require("http");
+const path = require("path");
+const express = require("express");
 const app = require("./src/app");
 const { initSocketServer } = require("./src/socket/socket.server");
 const connectionToDB = require("./src/db/db");
+
+// Serve frontend dist folder as static
+const publicPath = path.join(__dirname, "public");
+app.use(express.static(publicPath));
+
+// Fallback for React Router
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
 const httpServer = http.createServer(app);
 initSocketServer(httpServer);
--- frontend/src/context/ChatContext.jsx
+++ frontend/src/context/ChatContext.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 export const ChatContext = createContext();
 
 export const ChatProvider = ({ children }) => {
-  const BACKEND_URL = "http://localhost:4000/api";
+  const BACKEND_URL = "https://chatgptclone-ws4s.onrender.com/api";
   const [user, setUser] = useState(() => {
     const savedUser = localStorage.getItem("user");
     return savedUser ? JSON.parse(savedUser) : null;
